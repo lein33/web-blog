271a4efa75b836cdb53f01801f616033ac05b3fa
lein33 <sleandrojoel@hotmail.com>
2023-04-16 18:36:50 -0500

    si

--- blogweb/blogweb/settings.py
+++ blogweb/blogweb/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blogweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogweb',
+        'USER': 'leandro',
+        'PASSWORD': '04.desnutryfy',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

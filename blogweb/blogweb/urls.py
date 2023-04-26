bc374b7f6ed789922919752f2c1d28bb217c874f
lein33 <sleandrojoel@hotmail.com>
2023-04-25 23:41:09 -0500

    template

--- blogweb/blogweb/urls.py
+++ blogweb/blogweb/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('proyecto/',include('proyectos.urls')),
+    path('proyecto/',admin.site.urls),
 ]

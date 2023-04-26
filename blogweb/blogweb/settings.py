d044568346007967b28bef7f9bf2df1580a47e20
lein33 <sleandrojoel@hotmail.com>
2023-04-25 23:43:06 -0500

    template

--- blogweb/blogweb/settings.py
+++ blogweb/blogweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4*&kjfx!e))mhb#!&c0o!^24t^aade(9kv2x$qu62_f7p3@vn#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cod-evolucion.site','*']
 

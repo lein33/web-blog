2b019a09024779edcc7dcad5db41db333e1b3b67
lein33 <sleandrojoel@hotmail.com>
2023-04-28 22:42:09 -0500

    templates

--- blogweb/proyectos/views.py
+++ blogweb/proyectos/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def myfirstview(request):
-    return render(request,'index.html')
+    persona={
+        'name':'leandro'
+    }
+    return render(request,'index.html',persona)

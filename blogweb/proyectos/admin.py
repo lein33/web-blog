f49640a719fcd9cd3435a3133da1851c08658aeb
lein33 <sleandrojoel@hotmail.com>
2023-05-01 14:32:54 -0500

    agregando home

--- blogweb/proyectos/admin.py
+++ blogweb/proyectos/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Categoria
+from .models import *
 admin.site.register(Categoria)
+admin.site.register(Tag)
+admin.site.register(Autor)
+admin.site.register(Manual)
+
 # Register your models here.
